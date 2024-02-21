548bbd69f9478c6c8ef254cc9c855c5f1dc05fe5
Jack White <jfw793@gmail.com>
2024-02-21 20:17:39 +1100

    add environment variable to configmap and node

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
-app.get('/', (req, res) => {
- res.json("Hello world!");
+
+const NOUN = process.env.NOUN; // || "world";
+
+app.get("/", (req, res) => {
+  res.json(`Hello ${NOUN}!`);
 });
 
 app.listen(3000, () => {
- console.log('Server started on port 3000');
+  console.log("Server started on port 3000");
 });
